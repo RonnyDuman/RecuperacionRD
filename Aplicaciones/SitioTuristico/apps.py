bc9c6814e7f2ee13659d1924e843386583def7bf
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-25 04:38:02 -0500

    Configuramos settings.py y agregamos nuestra aplicacion

--- Aplicaciones/SitioTuristico/apps.py
+++ Aplicaciones/SitioTuristico/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SitioturisticoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SitioTuristico'
+    name = 'Aplicaciones.SitioTuristico'
