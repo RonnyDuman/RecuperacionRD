dbad2c843a0f527bcccb85d357ea551caa23f2cf
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-25 04:39:59 -0500

    Realizamos las migraciones correspondientes

--- recuperacionRD/settings.py
+++ recuperacionRD/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'recuperacionRD.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'sitio.sqlite3',
     }
 }
 
