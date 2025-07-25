1cfb8a10754a99737e63c81398b3f206eff617a9
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-25 01:42:14 -0500

    Creamos el modelo SitioTuristico

--- Aplicaciones/SitioTuristico/models.py
+++ Aplicaciones/SitioTuristico/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+# Creamos el modelo SitioTuristico
+class SitioTuristico(models.Model):
+    pais = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    requiere_visa = models.BooleanField(default=False)
+    foto_principal = models.FileField(upload_to='sitios/fotos_principales/')
+    foto_secundaria = models.FileField(upload_to='sitios/fotos_secundarias/')
+    historia_pdf = models.FileField(upload_to='sitios/historias/')
+    fecha_fundacion = models.DateField()
+    email_contacto = models.EmailField()
+    telefono_contacto = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
